d7d8b3e91278bb8cd4eb1b6a4cbdd0e085d97362
Kanchana Sondee <kangana512@gmail.com>
2021-05-25 01:08:09 +0700

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,58 @@
+export default {
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'nuxtjs-tailwind',
+        htmlAttrs: {
+            lang: 'en'
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/typescript
+        '@nuxt/typescript-build',
+        // https://go.nuxtjs.dev/tailwindcss
+        '@nuxtjs/tailwindcss',
+    ],
+    tailwindcss: {
+        cssPath: '~/assets/css/tailwind.css',
+        configPath: 'tailwind.config.js',
+        exposeConfig: false,
+        jit: true,
+        config: {
+            purge: {
+                content: [
+                    `components/**/*.{vue,js,ts}`,
+                    `layouts/**/*.vue`,
+                    `pages/**/*.vue`,
+                    `plugins/**/*.{js,ts}`,
+                    `nuxt.config.{js,ts}`
+                ]
+            }
+        }
+    },
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [],
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+        postcss: {}
+    }
+}
